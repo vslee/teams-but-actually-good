d5a6f777eb03891a3a9546d1453ad21e8b17a442
LeonimusT <leonimustytb@gmail.com>
2026-03-12 09:16:45 +0100

    fix: improve version input handling in release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-VERSION=$1  # ex: ./scripts/release.sh 1.2.0
+set -e
+
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+  echo "Usage: ./scripts/release.sh <version>"
+  exit 1
+fi
 
 sed -i '' "s/^version = .*/version = \"$VERSION\"/" src-tauri/Cargo.toml
-
 sed -i '' "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" src-tauri/tauri.conf.json
 
-# Commit + tag
-git add src-tauri/Cargo.tom src-tauri/tauri.conf.json
+git add src-tauri/Cargo.toml src-tauri/tauri.conf.json
 git commit -m "chore: release v$VERSION"
 git tag "v$VERSION"
 git push origin main "v$VERSION"
