ea5be3f7fabf951a047daca553e9f9a53bff53ed
LeonimusT <leonimustytb@gmail.com>
2026-03-12 10:30:27 +0100

    fix: correct version number to "0.0.1" in package.json and update release script to include package.json

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,8 +11,9 @@ fi
 
 sed -i '' "s/^version = .*/version = \"$VERSION\"/" src-tauri/Cargo.toml
 sed -i '' "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" src-tauri/tauri.conf.json
+sed -i '' "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" package.json
 
-git add src-tauri/Cargo.toml src-tauri/tauri.conf.json
+git add src-tauri/Cargo.toml src-tauri/tauri.conf.json package.json
 git commit -m "chore: release v$VERSION"
 git tag "v$VERSION"
 git push origin main "v$VERSION"
