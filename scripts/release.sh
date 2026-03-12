4e0d887e3a3e75ba203517561d7efa91a00266df
LeonimusT <leonimustytb@gmail.com>
2026-03-12 09:11:19 +0100

    fix: update target architecture for macOS and set version in tauri.conf.json

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,11 +2,12 @@
 
 VERSION=$1  # ex: ./scripts/release.sh 1.2.0
 
-# Met à jour Cargo.toml
 sed -i '' "s/^version = .*/version = \"$VERSION\"/" src-tauri/Cargo.toml
 
+sed -i '' "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" src-tauri/tauri.conf.json
+
 # Commit + tag
-git add src-tauri/Cargo.toml
+git add src-tauri/Cargo.tom src-tauri/tauri.conf.json
 git commit -m "chore: release v$VERSION"
 git tag "v$VERSION"
 git push origin main "v$VERSION"
