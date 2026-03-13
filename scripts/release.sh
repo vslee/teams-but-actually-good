d2d062969550e2730feecde31413d3d27e3050e9
LeonimusT <leonimustytb@gmail.com>
2026-03-13 09:33:05 +0100

    fix: update release script to include all extension manifest files for versioning

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,8 +12,12 @@ fi
 sed -i '' "s/^version = .*/version = \"$VERSION\"/" src-tauri/Cargo.toml
 sed -i '' "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" src-tauri/tauri.conf.json
 sed -i '' "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" package.json
+sed -i '' "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" extension/manifest.json
+sed -i '' "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" extension/manifest.chrome.json
+sed -i '' "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" extension/manifest.firefox.json
+sed -i '' "s/\"version\": \".*\"/\"version\": \"$VERSION\"/" extension/manifest.safari.json
 
-git add src-tauri/Cargo.toml src-tauri/tauri.conf.json package.json
+git add src-tauri/Cargo.toml src-tauri/tauri.conf.json package.json extension/manifest.json extension/manifest.chrome.json extension/manifest.firefox.json extension/manifest.safari.json
 git commit -m "chore: release v$VERSION"
 git tag "v$VERSION"
 git push origin main "v$VERSION"
