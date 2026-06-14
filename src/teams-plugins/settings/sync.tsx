de5c4d0ef531462e282b0426552b93c7923673f2
LeonimusTTV <leonimustytb@gmail.com>
2026-06-14 09:57:54 +0200

    fix: update sync documentation link in Sync component

--- src/teams-plugins/settings/sync.tsx
+++ src/teams-plugins/settings/sync.tsx
@@ -228,7 +228,7 @@ export default function Sync({ ReactLib }: { ReactLib: typeof React }) {
         <span className="tbg-setting-restart">
           If you've any question on how the syncing works, please refer to the{" "}
           <a
-            href="https://docs.teamsbutactuallygood.dev/TODO"
+            href="https://docs.teamsbutactuallygood.dev/sync"
             target="_blank"
             rel="noopener noreferrer"
             style={{ textDecoration: "underline", color: "inherit" }}
